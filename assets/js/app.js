c036afa9247bec5ba9a08aa5c02f05dc37898169
Rex Feng <rexfeng@gmail.com>
2015-02-16 13:43:16 -0800

    add jquery and keyboardjs to detect key presses in US locale

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,34 @@
-console.log('hi');
+var $ = require('jquery');
+var Keyboard = require('keyboardjs');
+
+$(function() {
+
+  Keyboard.on('a', function(e) {
+    console.log('you pressed a');
+  });
+
+  Keyboard.on('s', function(e) {
+    console.log('you pressed s');
+  });
+
+  Keyboard.on('d', function(e) {
+    console.log('you pressed d');
+  });
+
+  Keyboard.on('z', function(e) {
+    console.log('you pressed z');
+  });
+
+  Keyboard.on('x', function(e) {
+    console.log('you pressed x');
+  });
+
+  Keyboard.on('c', function(e) {
+    console.log('you pressed c');
+  });
+
+  Keyboard.on('space', function(e) {
+    console.log('you pressed space');
+  });
+
+});
